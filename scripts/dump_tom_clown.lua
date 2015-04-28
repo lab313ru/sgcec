9e7485852992ce52525c2c75598ab660a0fe7d26
DrMefistO <newinferno@gmail.com>
2015-04-29 01:56:42 +0300

    Fixed script for dumping Tom Clown.

--- scripts/dump_tom_clown.lua
+++ scripts/dump_tom_clown.lua
@@ -1,23 +1,21 @@
---local name = rom.saveto();
---local f = io.open(name, "wb+");
 dev.reset();
 
 for m = 0, 0xFF do
-	rom.write(0x402000, 4);
+rom.write_byte(0x402000, 4);
 end;
 
 for m = 0, 0xFF do
-	rom.write(0x404000, 0);
+rom.write_byte(0x404000, 0);
 end
 for m = 0, 0xFF do
-	rom.write(0x400000, 1);
+rom.write_byte(0x400000, 1);
 end
 
-rom.read(0, 4);
-rom.read(4, 4);
+--print(rom.read_word(0x2000, 0x100, true), ascii);
 
-print(rom.read(0x2000, 0x200, true), ascii);
+local name = "test_dump.bin";
+local f = io.open(name, "wb+");
 
---f:write(rom.read(0x0, 0x80000, true));
---f:close();
---print(string.format("Successfully dumped to: %s", name));
+f:write(rom.read_word(0, 0x40000, true));
+f:close();
+print(string.format("Successfully dumped to: %s", name));
